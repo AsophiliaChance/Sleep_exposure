4cf1f7cde3e2a0f864f3f5fbb60cbd9a3217901a
Qin <146370049+AsophiliaChance@users.noreply.github.com>
2026-09-01 00:19:52 +0300

    Update SL06LmmTtestFigure2B.m

--- SL06LmmTtestFigure2B.m
+++ SL06LmmTtestFigure2B.m
@@ -9,10 +9,10 @@
 %
 % 模型比较：
 %   Model 0:
-%       Y ~ Days * DeviantType + (1+ Days|Subjects)
+%       Y ~ Days * DeviantType + (1+ Subjects)
 %
 %   Model 1:
-%       Y ~ Days * DeviantType + Exposure + (1+ Days|Subjects)
+%       Y ~ Days * DeviantType + Exposure + (1+ Subjects)
 %
 %   使用 ML 比较两个模型；
 %   若 Exposure 模型显著改善拟合，则选择带 Exposure 的模型；
@@ -200,8 +200,8 @@ for m = 1:2
             continue;
         end
 
-        formula_noExp = 'Y ~ Days * DeviantType + (1+ Days|Subjects)';
-        formula_Exp   = 'Y ~ Days * DeviantType + Exposure + (1+ Days|Subjects)';
+        formula_noExp = 'Y ~ Days * DeviantType + (1+ Subjects)';
+        formula_Exp   = 'Y ~ Days * DeviantType + Exposure + (1+ Subjects)';
 
         lme_noExp_ML = fitlme(Tc_compare, formula_noExp, ...
             'FitMethod', 'ML', 'DummyVarCoding', dummyCoding);
